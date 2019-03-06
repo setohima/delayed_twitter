a1685d7aee2abf6e4d40fe1889e83497e9812e11
setohima <raito1261@gmail.com>
2019-03-07 01:04:33 +0900

    test

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    pass
+def fib1(n):
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  return fib1(n - 1) + fib1(n - 2)
 
-if __name__ == '__main__':
-    main()
+def fib2(n):
+  n0, n1 = 0, 1
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  for _ in range(n-1):
+    n0, n1 = n1, n0 + n1
+  return n1
 
+print('call fib1')
+for x in range(10):
+  print(f'{fib1(x)}, ', end='')
+print()
+print('call fib2')
+for x in range(10):
+  print(f'{fib2(x)}, ', end='')
